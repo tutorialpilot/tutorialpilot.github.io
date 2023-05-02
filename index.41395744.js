16c08a07f2734d511f4e407c2b8e0e21ae1e8029
Jasper <abideautomationx@gmail.com>
2023-05-02 19:30:53 +0530

    rtrhr

---
+++ index.41395744.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".fab").click((function(){$(this).find("i").toggleClass("fa-times fa-comment"),$(".floatingMenu").toggleClass("open"),$(".floatingMenu li").each((function(e,n){var i=$(".floatingMenu");i.queue((function(){$(n).toggleClass("visible"),i.dequeue()})).delay(50)}))}))}));
+//# sourceMappingURL=index.41395744.js.map
---
+++ index.7b5f18bf.js
@@ -0,0 +1,2 @@
+$.getJSON("../config.json",(function(e){e.secret_key}));
+//# sourceMappingURL=index.7b5f18bf.js.map
