92e9457aa37b3b25a0dbdf2ee099c13f5b94d1b8
Jasper <abideautomationx@gmail.com>
2023-05-07 08:51:06 +0530

    scsc

--- assets/TutorialOne-c44a91c4.js
+++
@@ -1,1 +0,0 @@
-import{j as i}from"./index-8f3f92da.js";function t(){return i.jsxs("div",{children:[i.jsx("h1",{children:"Tutorial One"}),i.jsx("p",{children:"This is tutorial one."})]})}export{t as default};
--- assets/TutorialTwo-630cfab9.js
+++
@@ -1,1 +0,0 @@
-import{j as i}from"./index-8f3f92da.js";function t(){return i.jsxs("div",{children:[i.jsx("h1",{children:"Tutorial Two"}),i.jsx("p",{children:"This is tutorial two."})]})}export{t as default};
